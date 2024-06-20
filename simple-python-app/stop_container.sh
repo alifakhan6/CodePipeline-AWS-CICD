bad954afc534c0fbeb86284cc4c2a357ba26edcc
nishankkoul <koulnishank5@gmail.com>
2024-06-20 15:53:42 +0530

    Update stop_container.sh

--- simple-python-app/stop_container.sh
+++ simple-python-app/stop_container.sh
@@ -2,7 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-containerID=docker ps | awk -F " " 'print $1'
-docker stop $containerID
-docker rm $containerID
-
+containerID=`docker ps | awk -F " " 'print $1'`
+docker rm -f $containerID
