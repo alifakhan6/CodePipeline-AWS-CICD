45585d0394b5c2405dc4f6c2cdf58b9792d1eade
nishankkoul <koulnishank5@gmail.com>
2024-06-20 16:33:09 +0530

    Update

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,4 +2,11 @@
 set -e
 
 # Stop and remove the running container (if any)
-echo "Hi"
+containerID=$(docker ps -q --filter "name=simple-python-flask-app")
+if [ -n "$containerID" ]; then
+  echo "Stopping and removing container $containerID"
+  docker stop "$containerID"
+  docker rm "$containerID"
+else
+  echo "No running container found with the name simple-python-flask-app"
+fi
