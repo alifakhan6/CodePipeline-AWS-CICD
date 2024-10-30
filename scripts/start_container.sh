26538c8b668bf823aad677a0421a5c0a7433b1f6
alifakhan6 <alifa.khan13@gmail.com>
2024-10-30 23:41:59 +0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 set -e
 
+# Define the port
+HOST_PORT=8000
+CONTAINER_PORT=5000
+IMAGE_NAME="alifa666/simple-python-flask-app"
+
 # Pull the Docker image from Docker Hub
-docker pull nishankkoul/simple-python-flask-app
+docker pull $IMAGE_NAME
+
+# Check if a container is already running on the host port
+EXISTING_CONTAINER=$(docker ps -q --filter "publish=$HOST_PORT")
+
+if [ -n "$EXISTING_CONTAINER" ]; then
+    echo "Stopping existing container using port $HOST_PORT..."
+    docker stop $EXISTING_CONTAINER
+    echo "Removing existing container..."
+    docker rm $EXISTING_CONTAINER
+fi
 
 # Run the Docker image as a container
-docker run -d -p 8000:5000 nishankkoul/simple-python-flask-app
-
-
+echo "Starting a new container..."
+docker run -d -p $HOST_PORT:$CONTAINER_PORT $IMAGE_NAME
+echo "Container started on port $HOST_PORT."
